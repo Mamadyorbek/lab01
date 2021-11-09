276f6f9e3eea59e0dc46b850d2d7e4773bba23e9
Mamadyorbek <93636782+Mamadyorbek@users.noreply.github.com>
2021-11-09 16:05:12 +0500

    Add files via upload
    Problem#2

---
+++ AMessageInARectangle.java
@@ -0,0 +1,9 @@
+package a.message.in.a.rectangle;
+public class AMessageInARectangle {
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*I like programming!*");
+        System.out.println("*********************");
+    }
+    
+}
